973e4feeeca77bd3a8cc6809f7cb7efad6c90b5e
f-yuya <yuya.fukunaga.dev+github@gmail.com>
2023-11-23 08:18:45 +0000

    [FIX] 最後に選ばれたメンバーも含まれるように修正します。

--- index.ts
+++ index.ts
@@ -26,24 +26,42 @@ const members: Member[] = JSON.parse(fs.readFileSync("./members.json", "utf8"));
  */
 const execute = (members: Member[]): Member[] => {
   const [from, ...others] = members;
-  if (!others.length) return [];
+  if (!others.length) return [from];
 
-  const to = others.find((member) => !from.exclusions.includes(member.no));
-  if (!to) throw new Error("組み合わせが見つかりません。");
+  const to = others.find((member) => isValidPair(from, member));
+  if (!to) throw new Error("組み合わせを生成できません。");
 
   const rest = others.filter((member) => member.no !== to.no);
   return [from, ...execute([to, ...rest.shuffle()])];
 };
 
 /**
+ * 組み合わせが正しいか検証します。
+ * @param from あげるメンバー
+ * @param to もらうメンバー
+ * @returns 検証に成功した場合は true、失敗した場合は false
+ */
+const isValidPair = (from: Member, to: Member) =>
+  !from.exclusions.includes(to.no);
+
+/**
+ * メンバーのリストをペアの形に変換します。
+ * @param members メンバーのリスト
+ * @returns ペア
+ */
+const toPair = (members: Member[]) => zip(members, members.shiftLeft());
+
+/**
  * エントリーポイント
  */
 const main = () => {
-  const result = execute(members.shuffle());
+  const result = toPair(execute(members.shuffle()));
 
-  zip(result, result.shiftLeft()).forEach(([from, to]) =>
-    console.log(`${from.name} → ${to.name}`),
-  );
+  // 最後のペアが正しくない場合があるため、検証する。
+  if (result.some(([from, to]) => !isValidPair(from, to)))
+    throw new Error("組み合わせを生成できません。");
+
+  result.forEach(([from, to]) => console.log(`${from.name} → ${to.name}`));
 };
 
 retry(main, 10);
